0447f46cf6efd059787cd9accc05b3fd1f163890
AlaBuveidani <Ala.buv@gmail.com>
2025-11-23 20:39:26 +0300

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import Dataset
+
+class TabularDataset(Dataset):
+    def __init__(self, X_num, X_cat, y):
+        self.X_num = torch.tensor(X_num, dtype=torch.float32)
+        self.X_cat = torch.tensor(X_cat, dtype=torch.long)
+        self.y = torch.tensor(y, dtype=torch.long)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.X_num[idx], self.X_cat[idx], self.y[idx]
